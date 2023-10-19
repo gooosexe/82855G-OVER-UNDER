40f10e529b9d00e4d2e4f14d97e04acdee65f733
Oliver <olivershaohuang2006@gmail.com>
2023-10-19 14:53:47 -0400

    removed claw code

--- OVER-UNDER/include/electronics.h
+++ OVER-UNDER/include/electronics.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "main.h"
 
-extern pros::Motor mtr_lf, mtr_lb, mtr_lbh, mtr_rf, mtr_rb, mtr_rbh;
+extern pros::Motor mtr_lf, mtr_lb, mtr_lbh, mtr_rf, mtr_rb, mtr_rbh, mtr_claw;
+extern pros::ADIDigitalOut clawLeft, clawRight;
+
 extern pros::Controller master;
 extern pros::Motor_Group left_drive, right_drive;
