ef246949a372fd251de7390fd96f2ebbccd5776f
Oliver <olivershaohuang2006@gmail.com>
2023-11-24 10:51:16 -0500

    changed tabs to spaces lol

--- OVER-UNDER/src/main.cpp
+++ OVER-UNDER/src/main.cpp
@@ -17,10 +17,10 @@ D: Proportional to the derivative of the position from the goal (velocity)
 // extern const lv_img_dsc_t funiiimage;
 
 void initialize() {
-	pros::lcd::initialize();
-	/*lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
-	lv_img_set_src(img1, &funiiimage);
-	lv_obj_align(img1, NULL, LV_ALIGN_CENTER, 0, 0);*/
+    pros::lcd::initialize();
+    /*lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
+    lv_img_set_src(img1, &funiiimage);
+    lv_obj_align(img1, NULL, LV_ALIGN_CENTER, 0, 0);*/
 }
 
 void disabled() {}
@@ -32,11 +32,11 @@ void competition_initialize() {}
  * Autonomous code.
  */
 void autonomous() {
-	bool autonSwitch = true;
+    bool autonSwitch = true;
 
-	// match auton
-	if (autonSwitch) moveStraight(48);
-	else mtr_flywheel = -127;
+    // match auton
+    if (autonSwitch) moveStraight(48);
+    else mtr_flywheel = -127;
 }
 
 /**
@@ -44,22 +44,22 @@ void autonomous() {
  * Driver control.
  */
 void opcontrol() {
-	while (true) {
-		// DRIVETRAIN
-		double ymotion = master.get_analog(ANALOG_LEFT_Y);
-		// quadratic turning	
-		double rotation = rotationCoefficient * pow(master.get_analog(ANALOG_RIGHT_X), 3);
-		
-		left_drive = (ymotion + rotation) * drivePower;
-		right_drive = (ymotion - rotation) * drivePower;
+    while (true) {
+        // DRIVETRAIN
+        double ymotion = master.get_analog(ANALOG_LEFT_Y);
+        // quadratic turning    
+        double rotation = rotationCoefficient * pow(master.get_analog(ANALOG_RIGHT_X), 3);
+        
+        left_drive = (ymotion + rotation) * drivePower;
+        right_drive = (ymotion - rotation) * drivePower;
 
-		// WINGS
-		wings.set_value(master.get_digital(DIGITAL_L2));
-		// INTAKE
-		intake.set_value(master.get_digital(DIGITAL_R2));
+        // WINGS
+        wings.set_value(master.get_digital(DIGITAL_L2));
+        // INTAKE
+        intake.set_value(master.get_digital(DIGITAL_R2));
 
-		// FLYWHEEL
-		if (master.get_digital(DIGITAL_B)) mtr_flywheel = -127;
-		else mtr_flywheel = 0;
-	}	
+        // FLYWHEEL
+        if (master.get_digital(DIGITAL_B)) mtr_flywheel = -127;
+        else mtr_flywheel = 0;
+    }    
 }
