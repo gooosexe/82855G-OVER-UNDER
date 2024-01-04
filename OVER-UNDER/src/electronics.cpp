694286f4a2e3cc7339e00e639c7794c31302228e
notez <viper18649@hotmail.com>
2024-01-03 22:07:00 -0500

    meow
    coded drivetrain, motor bindings, intake control

--- OVER-UNDER/src/electronics.cpp
+++ OVER-UNDER/src/electronics.cpp
@@ -1,18 +1,18 @@
 #include "electronics.h"
 
-pros::Motor mtr_lf(15, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_lf(3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
 pros::Motor mtr_lb(17, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_rf(7, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_rf(14, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 pros::Motor mtr_rb(9, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_lfh(14, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_rfh(6, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_flywheel(20, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_lfh(6, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_rfh(10, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_intake(1, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 pros::Motor_Group left_drive({mtr_lf, mtr_lb, mtr_lfh});
 pros::Motor_Group right_drive({mtr_rf, mtr_rb, mtr_rfh});
 
 pros::ADIDigitalOut wings('A', false);
-pros::ADIDigitalOut intake('C', false);
+
 pros::IMU imu_1(19);
 pros::IMU imu_2(16);
 
