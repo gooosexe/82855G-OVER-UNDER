efbfe6a7fa44d781c54f0828a9c3e9e7f6d55e0e
notez <32595884+8thnotez@users.noreply.github.com>
2024-01-16 16:34:55 -0500

    fixed flywheel port

--- OVER-UNDER/src/electronics.cpp
+++ OVER-UNDER/src/electronics.cpp
@@ -7,7 +7,7 @@ pros::Motor mtr_rb(9, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTA
 pros::Motor mtr_lfh(6, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 pros::Motor mtr_rfh(10, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
 pros::Motor mtr_intake(1, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_flywheel(2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_flywheel(20, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 pros::Motor_Group left_drive({mtr_lf, mtr_lb, mtr_lfh});
 pros::Motor_Group right_drive({mtr_rf, mtr_rb, mtr_rfh});
