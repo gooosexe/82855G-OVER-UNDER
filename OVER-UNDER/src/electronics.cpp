4de9f411e91f2fe14a106605a94ae40e1172556f
notez <32595884+8thnotez@users.noreply.github.com>
2024-02-17 09:24:09 -0500

    bruh

--- OVER-UNDER/src/electronics.cpp
+++ OVER-UNDER/src/electronics.cpp
@@ -1,11 +1,11 @@
 #include "electronics.h"
 
-pros::Motor mtr_lf(10, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_lb(9, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_rf(1, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_rb(2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_lbh(8, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor mtr_rbh(3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_lf(3, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_lb(1, pros::E_MOTOR_GEAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_rf(10, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_rb(9, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_lbh(2, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor mtr_rbh(8, pros::E_MOTOR_Ggit EAR_BLUE, true, pros::E_MOTOR_ENCODER_ROTATIONS);
 
 pros::Motor mtr_intake(4, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
 // pros::Motor mtr_flywheel(20, pros::E_MOTOR_GEAR_BLUE, false, pros::E_MOTOR_ENCODER_ROTATIONS);
